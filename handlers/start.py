7b0baff239468438c34f8bf9eff8fbf601532412
NewFate35 <dem.serega2001.sd@gmail.com>
2022-06-30 20:40:52 +0500

    приветствие по имени

--- handlers/start.py
+++ handlers/start.py
@@ -60,15 +60,11 @@ class Training(StatesGroup):
 async def new_chat_member(message: types.Message):
     last_msg = await db.select_last_msg_id()
 
-    text = f"Добро пожаловать в сообщество We|Run|Eda!"
-    "\nНажми на @weruneda_bot и изучи правила сообщества!"
+    text = f"Добро пожаловать в сообщество We|Run|Eda!\nНажми на @weruneda_bot и изучи правила сообщества!"
 
-    if message.from_user.first_name:
-        text = f"Добро пожаловать, {message.from_user.first_name}, " \
+    if message['new_chat_member']['first_name']:
+        text = f"Добро пожаловать, {message['new_chat_member']['first_name']}, " \
                f"в сообщество We|Run|Eda!\nНажми на @weruneda_bot и изучи правила сообщества!"
-    elif message.from_user.full_name:
-        text = f"Добро пожаловать, {message.from_user.full_name}, в сообщество We|Run|Eda!" \
-               f"\nНажми на @weruneda_bot и изучи правила сообщества!"
 
     if len(last_msg) > 0:
         await bot.delete_message(chat_id=message.chat.id, message_id=last_msg[0]['message_id'])
